2b2dfe3abe6edf09996a1742458baed2084173b3
Timothy Grant <timothyjgrant11@gmail.com>
2018-10-22 20:54:07 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,5 +9,12 @@ def reverse_each_word(string)
 new_array.join(" ")
   
 end
-
- #reverse using array.collect method 
+#reverse using array.collect method 
+def reverse_each_word_collect(string)
+  array = string.split
+  new_array = []
+  array.collect do |x|
+    new_array << x.reverse
+  end
+  new_array.join(" ")
+end
